6c3982b6be608b9ee6e8eb8ba379cda4a00a161e
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-10-03 13:35:04 -0700

    Use typeorm config to auto run migrations

--- charging-event/src/typeorm/typeorm.config.ts
+++ charging-event/src/typeorm/typeorm.config.ts
@@ -1,14 +1,15 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import Environment from '../config/env';
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import Environment from "../config/env";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
+  type: "postgres",
   host: Environment.DATABASE_HOST,
   port: Environment.DATABASE_PORT,
   username: Environment.DATABASE_USER,
   password: Environment.DATABASE_PASSWORD,
   database: Environment.DATABASE_NAME,
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['migrations/*.js'],
-  migrationsTableName: 'tbl_Migration_History_Charging_Event'
+  entities: ["dist/**/*.entity.js"],
+  migrations: ["migrations/*.js"],
+  migrationsTableName: "tbl_Migration_History_Charging_Event",
+  migrationsRun: true,
 };
--- user-management/src/typeorm/typeorm.config.ts
+++ user-management/src/typeorm/typeorm.config.ts
@@ -1,14 +1,15 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import Environment from '../config/env';
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import Environment from "../config/env";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
+  type: "postgres",
   host: Environment.DATABASE_HOST,
   port: Environment.DATABASE_PORT,
   username: Environment.DATABASE_USER,
   password: Environment.DATABASE_PASSWORD,
   database: Environment.DATABASE_NAME,
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['migrations/*.js'],
-  migrationsTableName: 'tbl_Migration_History_User_Management'
+  entities: ["dist/**/*.entity.js"],
+  migrations: ["migrations/*.js"],
+  migrationsTableName: "tbl_Migration_History_User_Management",
+  migrationsRun: true,
 };
