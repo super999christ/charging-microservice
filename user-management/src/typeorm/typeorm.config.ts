ce1237c07ff5ab25a745bf6225c39464c03e0f94
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-10-12 16:14:35 -0700

    Add migrationsRun to true

--- user-management/src/typeorm/typeorm.config.ts
+++ user-management/src/typeorm/typeorm.config.ts
@@ -3,11 +3,11 @@ import Environment from "../config/env";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: "postgres",
-  host: "stage-db-psql-charging-1.cizjbxfi5ccd.us-west-2.rds.amazonaws.com", //  Environment.DATABASE_HOST,
-  port: 5432, //Environment.DATABASE_PORT,
-  username: "postgres", // Environment.DATABASE_USER,
-  password: "MD2P!JcBlktnGz!P]y5LG?r}o5k[", //Environment.DATABASE_PASSWORD,
-  database: "nxuenergy", //Environment.DATABASE_NAME,
+  host: Environment.DATABASE_HOST,
+  port: Environment.DATABASE_PORT,
+  username: Environment.DATABASE_USER,
+  password: Environment.DATABASE_PASSWORD,
+  database: Environment.DATABASE_NAME,
   entities: ["dist/**/*.entity.js"],
   migrations: ["migrations/*.js"],
   migrationsTableName: "tbl_Migration_History_User_Management",
