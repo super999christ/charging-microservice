1c2c24f4a8d56f448b5f338859ddaa3ccd4072c5
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-10-19 15:13:32 -0700

    Fix pino logger issue for iot service

--- charging-event/src/config/env.ts
+++ charging-event/src/config/env.ts
@@ -1,24 +1,35 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
-dotenv.config({ path: '../../.env' });
+dotenv.config({ path: "../../.env" });
 
 const Environment = {
-  DATABASE_HOST: String(process.env['POSTGRES_HOST']),
-  DATABASE_PORT: Number(process.env['POSTGRES_PORT']),
-  DATABASE_USER: String(process.env['POSTGRES_USER']),
-  DATABASE_PASSWORD: String(process.env['POSTGRES_PASSWORD']),
-  DATABASE_NAME: String(process.env['POSTGRES_DB']),
-  SERVICE_USER_MANAGEMENT_URL: String(process.env['SERVICE_USER_MANAGEMENT_URL']),
-  SERVICE_NOTIFICATION_URL: String(process.env['SERVICE_NOTIFICATION_URL']),
-  SERVICE_API_AUTH_URL: String(process.env['SERVICE_API_AUTH_URL']),
-  SERVICE_PAYMENT_URL: String(process.env['SERVICE_PAYMENT_URL']),
-  SERVICE_CHARGING_IOT_URL: String(process.env['SERVICE_CHARGING_IOT_URL']),
-  SERVICE_CHARGING_IOT_CHECK_CON_URL: String(process.env['SERVICE_CHARGING_IOT_CHECK_CON_URL']),
-  SERVICE_CHARGING_IOT_MANAGE_CHG_URL: String(process.env['SERVICE_CHARGING_IOT_MANAGE_CHG_URL']),
-  SERVICE_CHARGING_IOT_COMPLETE_CHG_URL: String(process.env['SERVICE_CHARGING_IOT_COMPLETE_CHG_URL']),
-  PROMO1_FROM_DATE: new Date(process.env['PROMO1_FROM_DATE'] as string),
-  PROMO1_TO_DATE: new Date(process.env['PROMO1_TO_DATE'] as string),
+  DATABASE_HOST: String(process.env["POSTGRES_HOST"]),
+  DATABASE_PORT: Number(process.env["POSTGRES_PORT"]),
+  DATABASE_USER: String(process.env["POSTGRES_USER"]),
+  DATABASE_PASSWORD: String(process.env["POSTGRES_PASSWORD"]),
+  DATABASE_NAME: String(process.env["POSTGRES_DB"]),
+  SERVICE_USER_MANAGEMENT_URL: String(
+    process.env["SERVICE_USER_MANAGEMENT_URL"]
+  ),
+  SERVICE_NOTIFICATION_URL: String(process.env["SERVICE_NOTIFICATION_URL"]),
+  SERVICE_API_AUTH_URL: String(process.env["SERVICE_API_AUTH_URL"]),
+  SERVICE_PAYMENT_URL: String(process.env["SERVICE_PAYMENT_URL"]),
+  SERVICE_CHARGING_IOT_URL: String(process.env["SERVICE_CHARGING_IOT_URL"]),
+  SERVICE_CHARGING_IOT_CHECK_CON_URL: String(
+    process.env["SERVICE_CHARGING_IOT_CHECK_CON_URL"]
+  ),
+  SERVICE_CHARGING_IOT_MANAGE_CHG_URL: String(
+    process.env["SERVICE_CHARGING_IOT_MANAGE_CHG_URL"]
+  ),
+  SERVICE_CHARGING_IOT_COMPLETE_CHG_URL: String(
+    process.env["SERVICE_CHARGING_IOT_COMPLETE_CHG_URL"]
+  ),
+  PAYMENTS_PROCESSING_CRON_SCHEDULE: String(
+    process.env.PAYMENTS_PROCESSING_CRON_SCHEDULE
+  ),
+  PROMO1_FROM_DATE: new Date(process.env["PROMO1_FROM_DATE"] as string),
+  PROMO1_TO_DATE: new Date(process.env["PROMO1_TO_DATE"] as string),
 };
 
 export default Environment;
