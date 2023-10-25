d68ca66cde0b0e9e3f5b20598b69fd5951494d95
Randall Christ <randallchrist@nxuenergy.com>
2023-10-24 20:39:58 -0700

    [CA-240] investigate error log

--- charging-event/src/app/app.controller.ts
+++ charging-event/src/app/app.controller.ts
@@ -250,7 +250,6 @@ export class AppController {
       if (status.status != 0 && (status.chargeComplete != 0 || ["idle", "offline", "iot_error", "payment_error"].includes(status.sessionStatus))) {
         if (chargingEvent) {
           if (chargingEvent.sessionStatus && chargingEvent.sessionStatus !== 'in_progress') {
-            this.logger.error("Duplicated charging %o", chargingEvent);
             status.sessionStatus = chargingEvent.sessionStatus;
             status.sessionTotalCost = chargingEvent.totalCostDollars;
             response.send(status);
@@ -361,7 +360,6 @@ export class AppController {
           });
           try {
             if (chargingEvent.sessionStatus && chargingEvent.sessionStatus !== 'in_progress') {
-              this.logger.error("Duplicated charging %o", chargingEvent);
               result.error = "";
               response.send(result);
               return;
