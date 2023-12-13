53fd5d1a67cdfc778547f146cb1be0fad132bbd9
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-12-13 16:45:19 -0700

    Reformat log statement

--- charging-event/src/services/charging-iot/charging-iot.service.ts
+++ charging-event/src/services/charging-iot/charging-iot.service.ts
@@ -48,7 +48,7 @@ export class ChargingIoTService {
       } catch (err) {
         retryFlag = true;
 
-        this.logger.error(`IOT ${apiName}, retryCount ${retryCount}, retry ${count}, Status ${res.data.status}, response event ${(res.data as any).eventId}, msg  ${res.data}`);
+        this.logger.error(`IOT ${apiName}, retryCount ${retryCount}, retry ${count}, Status ${res.data.status}, Event ${(res.data as any).eventId}, Error  ${(res.data as any).error}`);
         this.logger.error(err);
       }
       if (retryFlag) {
